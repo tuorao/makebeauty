2d1344daaefcd15fc8798a36ed19a45400ab5ff0
tuorao <tuorao@naver.com>
2015-06-20 14:28:52 +0900

    다시시작

---
+++ M8/src/main/java/com/make/beauty/customer/vo/StoreVO.java
@@ -0,0 +1,27 @@
+/*
+* 작성일 : 2015. 6. 20.
+* 작성자 : Administrator
+*
+* 설명
+*/
+package com.make.beauty.customer.vo;
+
+import java.util.Date;
+
+import com.make.beauty.util.DateFormat;
+
+public class StoreVO {
+	
+	private int srl;
+	private String email;
+	private String password;
+	private String phone;
+	private String location;
+	private String division;
+	private int worker;
+	private String crn;
+	private String shortInfo;
+	private String detailInfo;
+	private DateFormat alterDate;
+
+}
---
+++ M8/src/main/java/com/make/beauty/util/DateFormat.java
@@ -0,0 +1,11 @@
+/*
+* 작성일 : 2015. 6. 20.
+* 작성자 : Administrator
+*
+* 설명
+*/
+package com.make.beauty.util;
+
+public class DateFormat {
+
+}
